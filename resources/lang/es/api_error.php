8f9b5dabaf486cb33f6954d9f17bdf94fc828cad
Jesus Alberto Gonzalez <jesusalbertogonzalez@MacBook-Air-de-Jesus.local>
2025-04-13 19:11:48 -0600

    actualizar api:errors

--- resources/lang/es/api_error.php
+++ resources/lang/es/api_error.php
@@ -4,16 +4,11 @@ return [
     'auth' => [
         'failed' => 'Upss, Correo o contraseña invalida'
     ],
-    'tenants' => [
-        'pre_store' => 'Ups, en este momento no es posible registrar el negocio, intentalo mas tarde'
-    ],
-    'cash_register' => [
-        'consult_branch' => 'Selecciona una sucursal para consultar la caja registradora',
-        'not_found' => 'Ups, No se encontro la caja registradora',
-        'not_assigned' => 'Ups, La sucursal no tiene una caja registradora asignada',
-        'not_open' => 'Ups, La caja registradora no esta abierta',
-        'not_close' => 'Ups, La caja registradora no esta cerrada',
-        'not_balance' => 'Ups, La caja registradora no esta balanceada',
-        'not_movement' => 'Ups, No se encontro el movimiento',
+    'users' => [
+        'index' => 'Error al obtener los usuarios, intentelo mas tarde',
+        'store' => 'Error al crear el usuario, intentelo mas tarde',
+        'show' => 'Error al obtener el usuario, intentelo mas tarde',
+        'update' => 'Error al actualizar el usuario, intentelo mas tarde',
+        'destroy' => 'Error al eliminar el usuario, intentelo mas tarde',
     ]
 ];
